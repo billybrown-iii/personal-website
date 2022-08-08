541e74856188db8805efc6bc689b39a992902e3c
Billy <billybdev@gmail.com>
2022-08-07 21:07:32 -0700

    styling for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import MDXWrapper from "../components/MDXWrapper";
 import InfoCard from "../components/InfoCard";
 
 const linkStyles = `
-w-full max-w-[12rem] my-5 mx-1 p-3 pr-2
+w-full max-w-[11.25rem] my-5 mx-2 p-3 pr-2
 border-2 border-gray-600 rounded-md
 bg-gray-600 text-gray-200
 hover:text-gray-50 hover:underline hover:decoration-emerald-500 hover:decoration-2
@@ -21,7 +21,7 @@ const Index = ({ data }) => {
           <InfoCard />
         </div>
 
-        <div className="w-4/5 md:w-2/3 md:pl-20 mb-20">
+        <div className="w-5/6 md:w-2/3 md:pl-20 mb-20">
           <MDXWrapper>
             <MDXRenderer>{data.mdx.body}</MDXRenderer>
           </MDXWrapper>
